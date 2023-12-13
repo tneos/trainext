ce441176294e714880684bf2982b6ebd10c2106f
Thomas Neos <tneos@hotmail.com>
2023-12-13 15:26:37 +0000

    Check if connected to cloud database

--- trainext-api/index.ts
+++ trainext-api/index.ts
@@ -3,8 +3,8 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-// import { DataSource } from 'typeorm';
-// import { Session } from './src/activities/activities.entity';
+import { DataSource } from 'typeorm';
+import { Session } from './src/activities/activities.entity';
 import { activitiesRouter } from './src/activities/activities.router';
 
 const mysql = require('mysql2');
@@ -31,35 +31,35 @@ const con = mysql.createConnection({
 });
 
 // Create Database connection -- TODO conditionally set synchronize property(true for development mode)
-//export const AppDataSource =
-// process.env.DB_HOST === '127.0.0.1'
-//   ? new DataSource({
-//       type: 'mysql',
-//       host: process.env.DB_HOST,
-//       port: 3200,
-//       username: process.env.MYSQL_USER,
-//       password: process.env.MYSQL_PASSWORD,
-//       database: process.env.MYSQL_DB,
-//       entities: [Session],
-//       synchronize: true,
-//     })
-//   :
-// new DataSource({
-//   type: 'mysql',
-//   url: process.env.MYSQL__ADDON_URI,
-//   logging: false,
-//   port: 3306,
-//   username: process.env.MYSQL_ADDON_USER,
-//   password: process.env.MYSQL_ADDON_PASSWORD,
-//   database: process.env.MYSQL_ADDON_DB,
-//   entities: [Session],
-//   synchronize: false,
-//   extra: {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+export const AppDataSource =
+  // process.env.DB_HOST === '127.0.0.1'
+  //   ? new DataSource({
+  //       type: 'mysql',
+  //       host: process.env.DB_HOST,
+  //       port: 3200,
+  //       username: process.env.MYSQL_USER,
+  //       password: process.env.MYSQL_PASSWORD,
+  //       database: process.env.MYSQL_DB,
+  //       entities: [Session],
+  //       synchronize: true,
+  //     })
+  //   :
+  new DataSource({
+    type: 'mysql',
+    url: process.env.MYSQL__ADDON_URI,
+    logging: false,
+    port: 3306,
+    username: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    entities: [Session],
+    synchronize: false,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
 // Define server port
 const port = process.env.MYSQL_ADDON_PORT;
@@ -69,13 +69,13 @@ app.listen(port, () =>
 );
 
 // Initialize typeORM
-// AppDataSource.initialize()
-//   .then(() => {
-//     // Start listening to requests on the defined port
-//     app.listen(port);
-//     console.log('Data source has been initialized');
-//   })
-//   .catch((err) => console.log(err));
+AppDataSource.initialize()
+  .then(() => {
+    // Start listening to requests on the defined port
+    app.listen(port);
+    console.log('Data source has been initialized');
+  })
+  .catch((err) => console.log(err));
 
 con.connect(function (err: any) {
   if (err) throw err;
