d0e196629e2b75a830dda43c244a3b39333475e7
Thomas Neos <tneos@hotmail.com>
2023-12-13 12:17:52 +0000

    Check if connected to cloud database

--- trainext-api/index.ts
+++ trainext-api/index.ts
@@ -36,7 +36,7 @@ export const AppDataSource =
       })
     : new DataSource({
         type: 'mysql',
-        host: process.env.DB_HOST,
+        host: process.env.DB__CLOUD_HOST,
         port: 3306,
         username: process.env.MYSQL_CLOUD_USER,
         password: process.env.MYSQL_CLOUD_PASSWORD,
