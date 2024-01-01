242f2f703ffd676f9a2552825ca115d88619fbed
Thomas Neos <tneos@hotmail.com>
2024-01-01 17:44:32 +0000

    Clear console messages

--- trainext-api/index.ts
+++ trainext-api/index.ts
@@ -40,6 +40,6 @@ AppDataSource.initialize()
     console.log('Data source has been initialized');
   })
   .catch((err) => console.log(err));
-console.log(AppDataSource);
+
 // Routes
 app.use('/', activitiesRouter);
